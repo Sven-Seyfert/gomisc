0f132ebb20909d302f20a2c589c1d9b21e271a95
Sven Seyfert <sven.seyfert@big-direkt.de>
2023-08-23 21:21:20 +0200

    Changed: File structure of single_instance.go and documents functions by comments.

--- singleinstance/single_instance.go
+++ singleinstance/single_instance.go
@@ -5,8 +5,18 @@ import (
 	"time"
 )
 
-const instanceFile = "instance_indicator"
+const instanceFile = "instance_indicator.lock"
 
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+// CreateInstanceFile creates a instance indicator file for this instance of
+// the program. To ensure this current instance of the program is the only one,
+// the function checks for a existing instance indicator file. In case there
+// is already a instance indicator file, this program will be exited.
 func CreateInstanceFile() {
 	if existsInstanceFile() {
 		os.Exit(0)
@@ -17,8 +27,8 @@ func CreateInstanceFile() {
 
 	defer file.Close()
 
-	// This 400 milliseconds delay ensures a robust file handling
-	time.Sleep(time.Millisecond * 400)
+	// This 150 milliseconds delay ensures a robust file handling
+	time.Sleep(time.Millisecond * 150)
 }
 
 func existsInstanceFile() bool {
@@ -30,13 +40,11 @@ func existsInstanceFile() bool {
 	return !fileInfo.IsDir()
 }
 
+// RemoveInstanceFile removes the previously created instance indicator file.
+// Always set this to ensure the next single instance of the program can be run.
+// If the instance indicator file still exists, you will not be able to run
+// your program because the exit procedure in CreateInstanceFile.
 func RemoveInstanceFile() {
 	err := os.Remove(instanceFile)
 	check(err)
-}
-
-func check(err error) {
-	if err != nil {
-		panic(err)
-	}
 }
